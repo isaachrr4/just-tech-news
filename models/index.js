348df094f74c2aa9e98b73df1cc64ada3f768da6
isaachrr4 <80857536+isaachrr4@users.noreply.github.com>
2021-07-20 13:07:38 -0400

    fixed bugs in backend of app

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./Users.js');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -55,4 +56,4 @@ User.hasMany(Post, {
     foreignKey: 'post_id'
   });
 
-  module.exports = { User, Post, Vote };
+  module.exports = { User, Post, Vote, Comment};
